58303934d16c7a5d19f026588f153dcd7fe59bd3
Aviral Singh <149692321+the-number-17@users.noreply.github.com>
2024-11-29 14:36:35 +0530

    Initial Commit

---
+++ Meal Plan Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Plan Project
+//
+//  Created by Batch - 1 on 29/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Meal Plan ProjectTests/Meal_Plan_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Plan_ProjectTests.swift
+//  Meal Plan ProjectTests
+//
+//  Created by Batch - 1 on 29/11/24.
+//
+
+import Testing
+@testable import Meal_Plan_Project
+
+struct Meal_Plan_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
